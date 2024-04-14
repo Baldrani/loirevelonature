23aba26192d39068890753f51d91e99a9654a0e8
Mael Mayon <mael.mayon@launchmetrics.com>
2024-04-14 19:30:59 +0200

    Fix ts

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -4,7 +4,7 @@ const Faq: React.FC<{ data: any }> = ({ data }) => {
         <section className="px-52 bg-zinc-950 pt-20 pb-36">
             <h2 className="text-4xl font-bold text-center mb-20">{title}</h2>
             <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-2">
-                {faq_cards.map(f => (
+                {faq_cards.map((f: any) => (
                     <FaqCard data={f} key={f.question} />
                 ))}
             </div>
