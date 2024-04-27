dc7c6003b13cef2a100ee168eac52af2536ad91a
Mael Mayon <mael.mayon@launchmetrics.com>
2024-04-28 00:01:23 +0200

    Improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: FC<{ data: TNavSection }> = ({ data }) => {
                 <div className="relative px-4 sm:px6 lg:px-8 flex h-16 items-center justify-between w-full">
                     <div className="flex items-center">
                         <Link href="/" className="ml-4 lg:ml-0">
-                            <h1 className="text-xl">{title} NAME</h1>
+                            <h1 className="text-xl">{title}</h1>
                         </Link>
                     </div>
                     <div className="flex space-x-3">
